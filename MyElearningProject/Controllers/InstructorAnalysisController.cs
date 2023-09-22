1fcb9202e7a0ac71e92a5dc28e44b80ac838c1ce
MuratYucedag <yucedagmurat23@gmail.com>
2023-09-22 22:08:42 +0300

    Add project files.

---
+++ MyElearningProject/Controllers/InstructorAnalysisController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyElearningProject.DAL.Context;
+
+namespace MyElearningProject.Controllers
+{
+    public class InstructorAnalysisController : Controller
+    {
+        ELearningContext context = new ELearningContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult InstructorPanelPartial()
+        {
+            int id = 1;
+            var values = context.Instructors.Where(x => x.InstructorID == id).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult CommentPartial()
+        {
+            //Select InstructorID from Instructors where Name='Ahmet' and Surname='Ölçen' = v1
+            var v1 = context.Instructors.Where(x => x.Name == "Ahmet" && x.Surname == "Ölçen").Select(y => y.InstructorID).FirstOrDefault();
+            
+            //Select CourseID From Courses Where InstructorID
+            var v2 = context.Courses.Where(x => x.InstructorID == v1).Select(y => y.CourseID).ToList();
+
+            //Select * From Comments Where CourseID In
+            var v3 = context.Comments.Where(x => v2.Contains(x.CourseID)).ToList();
+
+            return PartialView(v3);
+        }
+
+        public PartialViewResult CourseListByInstructor()
+        {
+            var values = context.Courses.Where(x => x.InstructorID == 1).ToList();
+            return PartialView(values);
+        }
+    }
+}
---
+++ MyElearningProject/DAL/Context/ELearningContext.cs
@@ -0,0 +1,20 @@
+﻿using MyElearningProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyElearningProject.DAL.Context
+{
+    public class ELearningContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseRegister> CourseRegisters { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
